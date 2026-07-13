442d62f3264b78e7a00ebd29b588b8a1014192c6
ersmax <masarin.eros@gmail.com>
2026-07-13 14:19:58 +0200

    add ex

--- src/Section2_CoreJava/L18_MultipleConditionalStatements/Main.java
+++ src/Section2_CoreJava/L18_MultipleConditionalStatements/Main.java
@@ -1,13 +1,16 @@
-package Section2_CoreJava.L17_ConditionalStatements;
+package Section2_CoreJava.L18_MultipleConditionalStatements;
 
 public class Main {
     public static void main(String[] args) {
         int x = 8;
         int y = 7;
+        int z = 9;
         System.out.print("The greates is ");
-        if (x > y)
+        if (x > y && x > z)
             System.out.println("x: " + x);
-        else
+        else if (y > z)
             System.out.println("y: " + y);
+        else
+            System.out.println("z: " + z);
     }
 }
