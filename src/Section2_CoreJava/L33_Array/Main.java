0a515d9b6dc9b909e4b98e667469be551ae71b3d
ersmax <masarin.eros@gmail.com>
2026-07-26 10:47:07 +0200

    add ex

--- src/Section2_CoreJava/L33_Array/Main.java
+++ src/Section2_CoreJava/L33_Array/Main.java
@@ -10,7 +10,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int nums[] = {3, 4, 5, 6};
+        int[] nums = {3, 4, 5, 6};
         System.out.println(nums[0]);
         nums[0] = 7;
         System.out.println(nums[0]);
