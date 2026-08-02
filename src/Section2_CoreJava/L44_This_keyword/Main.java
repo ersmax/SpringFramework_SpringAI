db2cb3f314e86e78a741a25b9f2f6ed1ff31c40f
ersmax <masarin.eros@gmail.com>
2026-08-02 19:18:35 +0200

    add ex

---
+++ src/Section2_CoreJava/L44_This_keyword/Main.java
@@ -0,0 +1,31 @@
+package Section2_CoreJava.L44_This_keyword;
+
+class Human {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Human aHuman = new Human();
+        aHuman.setAge(10);
+        System.out.println(aHuman.getAge());
+    }
+
+}
