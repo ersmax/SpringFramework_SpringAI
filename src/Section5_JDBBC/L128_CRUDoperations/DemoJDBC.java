4e7b151ba3a53365a5c96eebffe3f6a34fc021e1
ersmax <masarin.eros@gmail.com>
2026-08-24 20:16:54 +0200

    add ex

--- src/Section5_JDBBC/L128_CRUDoperations/DemoJDBC.java
+++ src/Section5_JDBBC/L128_CRUDoperations/DemoJDBC.java
@@ -9,8 +9,7 @@ public class DemoJDBC {
         System.out.println("SID\tName\tMarks");
         System.out.println("---\t----\t-----");
         while(resultSet.next()) {
-            int sid = 0;
-            sid = resultSet.getInt("sid");
+            int sid = resultSet.getInt("sid");
             String name = resultSet.getString("name");
             int marks = resultSet.getInt("marks");
             System.out.println(sid + "\t" + name + "\t" + marks);
@@ -49,29 +48,50 @@ public class DemoJDBC {
             showStudent(resultSet);
 
             /** CREATE query */
-            // 6. Execute statement query: create data
+            // 6. Execute statement query: insert data
             String insertSql = "INSERT INTO public.student VALUES (10, 'Alexei', 67)";
             // true if the first result is a ResultSet object (a result of Retrieve query);
             // false if it is an update count or there are no results
-            statement.execute(insertSql);
+//            boolean status = statement.execute(insertSql);
+//            System.out.println("Execute status: " + status); // false for INSERT/UPDATE/DELETE
+            // or we can use executeUpdate(insertSql), which returns either
+            // (1) the row count for SQL Data Manipulation Language (DML) statements or
+            // (2) 0 for SQL statements that return nothing
+            int rowsAffected = statement.executeUpdate(insertSql);
+            System.out.println("Rows affected by insert: " + rowsAffected);
+            System.out.println("Record inserted successfully");
             // 6b. Process result
             resultSet = statement.executeQuery(retrieveSql);    // return data
             showStudent(resultSet);
 
             /** UPDATE query */
+            // 6. Execute statement query: update data
             String updateSql = "UPDATE public.student SET name='The Loser' WHERE sid=10";
-            statement.execute(updateSql);
+//            statement.execute(updateSql);
+            int rowsUpdated = statement.executeUpdate(updateSql);
+            System.out.println("Rows updated: " + rowsUpdated);
             // 6b. Process result
             resultSet = statement.executeQuery(retrieveSql);    // return data
             showStudent(resultSet);
 
             /** DELETE query */
+            // 6. Execute statement query: delete data
             String deleteSql = "DELETE FROM public.student WHERE sid = 10";
-            statement.execute(deleteSql);
+//            statement.execute(deleteSql);
+            int rowsDeleted = statement.executeUpdate(deleteSql);
+            System.out.println("Rows deleted: " + rowsDeleted);
             // 6b. Process result
             resultSet = statement.executeQuery(retrieveSql);    // return data
             showStudent(resultSet);
 
+            /** RETRIEVE with execute */
+            // 6. Execute statement query: retrieve data
+            String select2Sql = "SELECT * FROM public.student WHERE sid = 7";
+            boolean found = statement.execute(select2Sql);
+            System.out.println("Result found: " + found);
+            // 6b. Process result
+            resultSet = statement.executeQuery(select2Sql);
+            showStudent(resultSet);
 
 
         } catch (ClassNotFoundException e) {
