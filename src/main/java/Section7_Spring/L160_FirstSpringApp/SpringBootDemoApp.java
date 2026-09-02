6543a8160e8a120a7de8fbb05f6ba738b5ddc4de
ersmax <masarin.eros@gmail.com>
2026-09-02 19:07:35 +0200

    Add First Spring App

---
+++ src/main/java/Section7_Spring/L160_FirstSpringApp/SpringBootDemoApp.java
@@ -0,0 +1,13 @@
+package Section7_Spring.L160_FirstSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApp.class, args);
+        System.out.println("Hello");
+    }
+}
---
+++ src/test/java/Section7_Spring/L160_FirstSpringApp/SpringBootDemoAppTests.java
@@ -0,0 +1,12 @@
+package Section7_Spring.L160_FirstSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootDemoAppTests {
+
+    @Test
+    void contestLoads() {
+    }
+}
