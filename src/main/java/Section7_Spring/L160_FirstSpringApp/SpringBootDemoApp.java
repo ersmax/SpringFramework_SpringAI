b04bbe9e67e738ce5f1c213e66daf93cb9c621d3
ersmax <masarin.eros@gmail.com>
2026-09-02 22:03:43 +0200

    Setting up Spring pom

--- src/main/java/Section7_Spring/L160_FirstSpringApp/SpringBootDemoApp.java
+++ src/main/java/Section7_Spring/L160_FirstSpringApp/SpringBootDemoApp.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootDemoApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootDemoApp.class, args);
+        SpringApplication.run(SpringBootApplication.class, args);
         System.out.println("Hello");
     }
 }
