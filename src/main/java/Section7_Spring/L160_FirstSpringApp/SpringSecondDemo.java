0b460a0a0653fbcdeb460436e1e2b798f114f778
ersmax <masarin.eros@gmail.com>
2026-09-02 20:21:48 +0200

    Add Second Spring App

---
+++ src/main/java/Section7_Spring/L160_FirstSpringApp/SpringSecondDemo.java
@@ -0,0 +1,12 @@
+package Section7_Spring.L160_FirstSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecondDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecondDemo.class, args);
+    }
+}
