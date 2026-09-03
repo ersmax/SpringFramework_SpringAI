4b6bbec746f71da484389e4d4d010cf4a82bd0eb
ersmax <masarin.eros@gmail.com>
2026-09-03 12:32:29 +0200

    Adding DI and IOC

---
+++ src/main/java/Section7_Spring/L162_Autowiring/Alien.java
@@ -0,0 +1,11 @@
+package Section7_Spring.L162_Autowiring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alien {
+
+    public void code() {
+        System.out.println("Coding");
+    }
+}
---
+++ src/main/java/Section7_Spring/L162_Autowiring/SpringBootDemoApplication.java
@@ -0,0 +1,21 @@
+package Section7_Spring.L162_Autowiring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(SpringBootDemoApplication.class, args);
+
+        Alien newAlien = new Alien();
+        newAlien.code();
+
+        Alien newAl = context.getBean(Alien.class);
+        newAl.code();
+
+        Alien newAl2 = context.getBean(Alien.class);
+        newAl2.code();
+    }
+}
